2790fb0766ae1f2e3f734a08c2a51d18c49793e7
HamzaNaveed19 <125171819+HamzaNaveed19@users.noreply.github.com>
2023-08-08 15:37:00 +0500

    Add files via upload

---
+++ LAB_14/22l-7871_t1.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//using namespace std;
+//
+//template <class type>
+//type surprise(type x, type y)
+//{
+//	return x + y;
+//}
+//int main() {
+//	cout << surprise(5, 7) << endl;    	//output : 12 
+//	 string str1 = "Sunny";
+//	string str2 = " Day";
+//	cout << surprise(str1, str2) << endl;     //output : Sunny Day
+//
+//}
