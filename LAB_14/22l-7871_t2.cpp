996fec58cbdd24a8c3c0b4fe05d541b6052ded4b
HamzaNaveed19 <125171819+HamzaNaveed19@users.noreply.github.com>
2023-08-09 20:04:40 +0500

    Add files via upload

---
+++ LAB_14/22l-7871_t2.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//using namespace std;
+//template <typename T>
+//T 	performOperation(T x,T y, char oper) {
+//	if (oper == '+') {
+//		return x + y;
+//	}
+//	if (oper == '-') {
+//		return x - y;
+//	}
+//	if (oper == '*') {
+//		return x * y;
+//	}
+//	if (oper == '/' && y!=0) {
+//		return x / y;
+//	}
+//	else {
+//
+//	}
+//}
+//
+//int main() 
+//{
+//	int a, b;
+//	char op;
+//	cout << "Enter first operand ";
+//	cin >> a;
+//	cout << "Enter second operand ";
+//	cin >> b;
+//	cout << "Enter operation ";
+//	cin >> op; // op can be +, -, * or /
+//	if (op == '*' || op == '+' || op == '-' || op == '/')
+//	{
+//		cout<<"Result : "<<performOperation(a, b, op);
+//	}
+//	else
+//	{
+//		cout << "Wrong operation";
+//	}
+//}
